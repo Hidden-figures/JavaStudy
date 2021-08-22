1ada891bed9a8f903404b690edfbc9ab7ad806a8
RiverJu <jiwoo99311@likelion.org>
2021-08-23 01:35:38 +0900

    [java]ch5 study

---
+++ Ch5/src/sec01/exam02/NullExample.java
@@ -0,0 +1,14 @@
+package sec01.exam02;
+
+public class NullExample {
+
+	public static void main(String[] args) {
+		String hobby = "cooking";
+		String name = "java";
+		name = null;
+		
+		System.out.println(hobby.length());
+
+	}
+
+}
